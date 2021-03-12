f5e3462f7fc7aa26e642861033caf24c12715319
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-03-12 11:46:07 -0800

    feat: Add images and data for memory cards

---
+++ src/data/arcana.js
@@ -0,0 +1,60 @@
+import chariot from '../images/P5_Chariot_arcana_cooperation.png';
+import emperor from '../images/P5_Emperor_arcana_cooperation.png';
+import empress from '../images/P5_Empress_arcana_cooperation.png';
+import fool from '../images/P5_Fool_arcana_cooperation.png';
+import hermit from '../images/P5_Hermit_Arcana.png';
+import judgement from '../images/P5_Judgement_Arcana.png';
+import justice from '../images/P5_Justice_Arcana.png';
+import magician from '../images/P5_Magician_arcana_cooperation.png';
+import priestess from '../images/P5_Priestess_arcana_cooperation.png';
+import strength from '../images/P5_Strength_Arcana.png';
+import world from '../images/P5_World_Arcana.png';
+
+const arcana = [
+    {
+        src: chariot,
+        alt: 'The Chariot tarot card',
+    },
+    {
+        src: emperor,
+        alt: 'The Emperor tarot card',
+    },
+    {
+        src: empress,
+        alt: 'The Empress tarot card',
+    },
+    {
+        src: fool,
+        alt: 'The Fool tarot card',
+    },
+    {
+        src: hermit,
+        alt: 'The Hermit tarot card',
+    },
+    {
+        src: judgement,
+        alt: 'The Judgement tarot card',
+    },
+    {
+        src: justice,
+        alt: 'The Justice tarot card',
+    },
+    {
+        src: magician,
+        alt: 'The Magician tarot card',
+    },
+    {
+        src: priestess,
+        alt: 'The Priestess tarot card',
+    },
+    {
+        src: strength,
+        alt: 'The Strength tarot card',
+    },
+    {
+        src: world,
+        alt: 'The World tarot card',
+    },
+];
+
+export default arcana;
