44a6921e663b70cf44ad63e9a0a3d35149242a8c
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-03-13 20:00:15 -0800

    feat: Add layout to ButtonGroup

--- src/components/molecules/ButtonGroup.jsx
+++ src/components/molecules/ButtonGroup.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const GroupContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    align-items: baseline;
+    justify-content: center;
+    font-size: 2.4rem;
+    column-gap: 1.2rem;
+
+    & a {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        width: 100%;
+        line-height: normal;
+    }
+
+    & > * {
+        padding: 0 1rem;
+    }
+`;
 
 const ButtonGroup = ({ children }) => {
-    return <div>{children}</div>;
+    return <GroupContainer>{children}</GroupContainer>;
 };
 
 export default ButtonGroup;
